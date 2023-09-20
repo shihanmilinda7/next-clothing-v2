976576f256fa59e5ae0a3fd350ea385af6867e9b
milinda <shihanmilinda7@gmail.com>
2023-09-20 13:59:23 +0530

    ssss

--- src/app/components/page-components/fabrics/addnew.tsx
+++ src/app/components/page-components/fabrics/addnew.tsx
@@ -118,7 +118,7 @@ const NewFabric = () => {
         onClick={() => setIsOpen(true)}
         className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
       >
-        Create New - F2
+        Create New - F3
       </button>
       <Modal
         isOpen={isOpen}
